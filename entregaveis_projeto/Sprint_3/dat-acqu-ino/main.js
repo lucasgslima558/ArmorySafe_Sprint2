7b5f0b3e6f288b95a3967e6e24c3cd85746f6b2d
gMedeiros23 <gabriel.nascimento@sptech.school>
2025-12-08 13:24:49 -0300

    Dat-acqu-ino - Exclusão do gráfico e movimentação para sprint 3

---
+++ entregaveis_projeto/Sprint_3/dat-acqu-ino/main.js
@@ -0,0 +1,117 @@
+// importa os bibliotecas necessárias
+const serialport = require('serialport');
+const express = require('express');
+const mysql = require('mysql2');
+
+// constantes para configurações
+const SERIAL_BAUD_RATE = 9600;
+const SERVIDOR_PORTA = 3300;
+
+// habilita ou desabilita a inserção de dados no banco de dados
+const HABILITAR_OPERACAO_INSERIR = true;
+
+// função para comunicação serial
+const serial = async (
+    valoresSensorUmidade
+) => {
+
+    // conexão com o banco de dados MySQL
+    let poolBancoDados = mysql.createPool(
+        {
+            host: 'localhost',
+            user: 'aluno',
+            password: 'Sptech#2024',
+            database: 'ArmorySafe',
+            port: 3307
+        }
+    ).promise();
+
+    // lista as portas seriais disponíveis e procura pelo Arduino
+    const portas = await serialport.SerialPort.list();
+    const portaArduino = portas.find((porta) => porta.vendorId == 2341 && porta.productId == 43);
+    if (!portaArduino) {
+        throw new Error('O arduino não foi encontrado em nenhuma porta serial');
+    }
+
+    // configura a porta serial com o baud rate especificado
+    const arduino = new serialport.SerialPort(
+        {
+            path: portaArduino.path,
+            baudRate: SERIAL_BAUD_RATE
+        }
+    );
+
+    // evento quando a porta serial é aberta
+    arduino.on('open', () => {
+        console.log(`A leitura do arduino foi iniciada na porta ${portaArduino.path} utilizando Baud Rate de ${SERIAL_BAUD_RATE}`);
+    });
+
+    // processa os dados recebidos do Arduino
+    arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
+        console.log(data);
+        const valorUmidade = parseFloat(data);
+        const dtCaptura = new Date().toLocaleString('sv-SE', { timeZone: 'America/Sao_Paulo' })
+        
+       
+        // armazena os valores dos sensores nos arrays correspondentes
+        valoresSensorUmidade.push(valorUmidade);
+        
+        // insere os dados no banco de dados (se habilitado)
+        if (HABILITAR_OPERACAO_INSERIR) {
+
+            // este insert irá inserir os dados na tabela "medida"
+            await poolBancoDados.execute(
+                'INSERT INTO dadosArduino (fkArduino, umidade, dtCaptura) VALUES (1, ?, ?)',
+                [valorUmidade, dtCaptura]
+            );
+            console.log("valores inseridos no banco: " +  valorUmidade + " " + dtCaptura);
+
+        }
+
+    });
+
+    // evento para lidar com erros na comunicação serial
+    arduino.on('error', (mensagem) => {
+        console.error(`Erro no arduino (Mensagem: ${mensagem}`)
+    });
+}
+
+// função para criar e configurar o servidor web
+const servidor = (
+    valoresSensorUmidade,
+) => {
+    const app = express();
+
+    // configurações de requisição e resposta
+    app.use((request, response, next) => {
+        response.header('Access-Control-Allow-Origin', '*');
+        response.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+        next();
+    });
+
+    // inicia o servidor na porta especificada
+    app.listen(SERVIDOR_PORTA, () => {
+        console.log(`API executada com sucesso na porta ${SERVIDOR_PORTA}`);
+    });
+
+    // define os endpoints da API para cada tipo de sensor
+    app.get('/sensores/analogico', (_, response) => {
+        return response.json(valoresSensorUmidade);
+    });
+}
+
+// função principal assíncrona para iniciar a comunicação serial e o servidor web
+(async () => {
+    // arrays para armazenar os valores dos sensores
+    const valoresSensorUmidade = [];
+
+    // inicia a comunicação serial
+    await serial(
+        valoresSensorUmidade
+    );
+
+    // inicia o servidor web
+    servidor(
+        valoresSensorUmidade
+    );
+})();
